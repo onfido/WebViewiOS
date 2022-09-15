397a2cedd56ecf401c72dcd61c2d31847bb2747c
Pedro Henrique <pedro.henrique@onfido.com>
2022-09-15 13:57:13 +0100

    Initial Commit

---
+++ WebViewiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewiOS
+//
+//  Created by Pedro Henrique on 15/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
