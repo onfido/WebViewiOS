936b0c05702768b3ec0a0a412904cc9f04eb77d2
sergiomartins <sergio.martins@onfido.com>
2024-03-15 15:32:56 +0000

    Integration tests Web SDK 14+

---
+++ SmartCaptureDemoUITests/SmartCaptureDemoUITests.swift
@@ -0,0 +1,18 @@
+//
+//  SmartCaptureDemoUITests.swift
+//  SmartCaptureDemoUITests
+//
+//  Created by Sérgio de Oliveira Martins on 15/03/2024.
+//
+
+import XCTest
+
+final class SmartCaptureDemoUITests: XCTestCase {
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
